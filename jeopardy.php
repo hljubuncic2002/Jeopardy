e344db9d8f8009442301bf09a37d56cc9709c2ee
Haris Ljubuncic <99652252+hljubuncic2002@users.noreply.github.com>
2024-04-02 09:40:12 -0400

    Update jeopardy.php

--- jeopardy.php
+++ jeopardy.php
@@ -8,41 +8,66 @@
     <title>Document</title>
 </head>
 <body>
+  <?php
+    global $score1;
+    $questions=file_get_contents("data.txt");
+    $questionsList=explode(PHP_EOL, $questions);
+    $questionsArr = [];
+    foreach ($questionsList as $ques) {
+      $finalLines = explode(",", $ques);
+      array_push($questionsArr, $finalLines);
+    }
+    function score1(&$score1){
+      $score1+=100;
+    }
+
+    ?>
     <div class="game">
-
       <?php
-        if(array_key_exists('1200', $_POST)) { ?>
-          <div class="qCard">
-            <h1>What is the captial of Germany?</h1>
-            <form>
-              <input type="text" id="1200answer">
-            </form>
-          </div>
-          <?php
-        }
-       ?>
 
-      <form method="post">
+      if (isset($_POST['button'])) {
+        ?>
+        <div class="qCard">
+              <h1><?php
+              $qVal=($_POST['button']);
+              echo($questionsArr[$qVal][0]);
+              ?>
+            </h1>
+              <form method="post" action="">
+                <input type="text" id="text" name="text">
+              </form>
+            </div> <?php
+            score1();
+      }
+
+      ?>
+        <div class="scores">
+            <h3> player 1: <?php echo $score1?> </h3>
+          <h3> player 2: <?php echo($score2) ?> </h3>
+      </div>
+
+
+      <form method="post" action="">
         <div class="grid">
 
-          <div class="gridBox">Topic 1</div>
-          <div class="gridBox">Topic 2</div>
-          <div class="gridBox">Topic 3</div>
+          <div class="gridBox">Capitals</div>
+          <div class="gridBox">Leaders</div>
+          <div class="gridBox">Countries</div>
           <div class="gridBox">Topic 4</div>
           <div class="gridBox">Topic 5</div>
           <div class="gridBox">Topic 6</div>
-          <button name="1200"><div class="gridBox">$200</div></button>
-          <div class="gridBox">$200</div>
-          <div class="gridBox">$200</div>
-          <div class="gridBox">$200</div>
-          <div class="gridBox">$200</div>
-          <div class="gridBox">$200</div>
-          <div class="gridBox">$400</div>
-          <div class="gridBox">$400</div>
-          <div class="gridBox">$400</div>
-          <div class="gridBox">$400</div>
-          <div class="gridBox">$400</div>
-          <div class="gridBox">$400</div>
+          <button name="button" value="0"><div class="gridBox">$200</div></button>
+          <button name="button" value="1"><div class="gridBox">$200</div></button>
+          <button name="button" value="2"><div class="gridBox">$200</div></button>
+          <button name="button" value="3"><div class="gridBox">$200</div></button>
+          <button name="button" value="4"><div class="gridBox">$200</div></button>
+          <button name="button" value="5"><div class="gridBox">$200</div></button>
+          <button name="button" value="6"><div class="gridBox">$400</div></button>
+          <button name="button" value="7"><div class="gridBox">$400</div></button>
+          <button name="button" value="8"><div class="gridBox">$400</div></button>
+          <button name="button" value="9"><div class="gridBox">$400</div></button>
+          <button name="button" value="10"><div class="gridBox">$400</div></button>
+          <button name="button" value="11"><div class="gridBox">$400</div></button>
           <div class="gridBox">$600</div>
           <div class="gridBox">$600</div>
           <div class="gridBox">$600</div>
@@ -64,6 +89,7 @@
 
         </div>
         </form>
+
 </div>
 </body>
 </html>
