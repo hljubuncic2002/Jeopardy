3d6e507ea28dde120112a7a5259432e8708a3f0d
Haris Ljubuncic <99652252+hljubuncic2002@users.noreply.github.com>
2024-04-07 14:43:38 -0400

    Update jeopardy.php

--- jeopardy.php
+++ jeopardy.php
@@ -1,15 +1,32 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel=stylesheet href="jeopardy.css">
-    <title>Document</title>
-</head>
-<body>
-  <?php
-    global $score1;
+<?php
+session_start();
+
+for ($i=1; $i <= $_SESSION['noUsers']; ++$i){
+  if (!(isset($_SESSION['player'.$i]))) {
+    $_SESSION['player'.$i]=$_POST['player'.$i];
+  }
+  if (!(isset($_SESSION['score'.$i]))) {
+      $_SESSION['score'.$i]=0;
+  }
+  if (!(isset($_SESSION['counter']))) {
+    $_SESSION['counter']=0;
+  }
+}
+ ?>
+
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="jeopardy.css">
+
+  </head>
+  <body>
+    <?php
+    if (!(isset($_SESSION['ansArr']))){
+      $_SESSION['ansArr']=array();
+    }
+
     $questions=file_get_contents("data.txt");
     $questionsList=explode(PHP_EOL, $questions);
     $questionsArr = [];
@@ -17,79 +34,47 @@
       $finalLines = explode(",", $ques);
       array_push($questionsArr, $finalLines);
     }
-    function score1(&$score1){
-      $score1+=100;
-    }
-
+    $_SESSION["questionsArr"]=$questionsArr;
     ?>
-    <div class="game">
-      <?php
+   <div class="game">
+     <div class="jeopardy">
+       <h2><b>Jeopardy!</b></h2>
+     </div>
+     <div class="scores">
+     <?php
+      $noUsers=$_SESSION['noUsers'];
+      for($i=1;$i<=$noUsers;$i++){ ?>
+         <div class="userScore">
+          <p> <?php echo $_SESSION['player'.$i], ': ', $_SESSION['score'.$i]; ?> </p>
+        </div>
+          <?php
+        }?>
+    </div>
 
-      if (isset($_POST['button'])) {
-        ?>
-        <div class="qCard">
-              <h1><?php
-              $qVal=($_POST['button']);
-              echo($questionsArr[$qVal][0]);
-              ?>
-            </h1>
-              <form method="post" action="">
-                <input type="text" id="text" name="text">
-              </form>
-            </div> <?php
-            score1();
-      }
-
-      ?>
-        <div class="scores">
-            <h3> player 1: <?php echo $score1?> </h3>
-          <h3> player 2: <?php echo($score2) ?> </h3>
-      </div>
-
-
-      <form method="post" action="">
+      <form action="Question.php" method="post">
         <div class="grid">
 
-          <div class="gridBox">Capitals</div>
-          <div class="gridBox">Leaders</div>
-          <div class="gridBox">Countries</div>
-          <div class="gridBox">Topic 4</div>
-          <div class="gridBox">Topic 5</div>
-          <div class="gridBox">Topic 6</div>
-          <button name="button" value="0"><div class="gridBox">$200</div></button>
-          <button name="button" value="1"><div class="gridBox">$200</div></button>
-          <button name="button" value="2"><div class="gridBox">$200</div></button>
-          <button name="button" value="3"><div class="gridBox">$200</div></button>
-          <button name="button" value="4"><div class="gridBox">$200</div></button>
-          <button name="button" value="5"><div class="gridBox">$200</div></button>
-          <button name="button" value="6"><div class="gridBox">$400</div></button>
-          <button name="button" value="7"><div class="gridBox">$400</div></button>
-          <button name="button" value="8"><div class="gridBox">$400</div></button>
-          <button name="button" value="9"><div class="gridBox">$400</div></button>
-          <button name="button" value="10"><div class="gridBox">$400</div></button>
-          <button name="button" value="11"><div class="gridBox">$400</div></button>
-          <div class="gridBox">$600</div>
-          <div class="gridBox">$600</div>
-          <div class="gridBox">$600</div>
-          <div class="gridBox">$600</div>
-          <div class="gridBox">$600</div>
-          <div class="gridBox">$600</div>
-          <div class="gridBox">$800</div>
-          <div class="gridBox">$800</div>
-          <div class="gridBox">$800</div>
-          <div class="gridBox">$800</div>
-          <div class="gridBox">$800</div>
-          <div class="gridBox">$800</div>
-          <div class="gridBox">$1000</div>
-          <div class="gridBox">$1000</div>
-          <div class="gridBox">$1000</div>
-          <div class="gridBox">$1000</div>
-          <div class="gridBox">$1000</div>
-          <div class="gridBox">$1000</div>
+          <div class="gridBoxTop">Capitals</div>
+          <div class="gridBoxTop">Leaders</div>
+          <div class="gridBoxTop">Countries</div>
+          <div class="gridBoxTop">Animals</div>
+          <div class="gridBoxTop">Computing</div>
+          <div class="gridBoxTop">Cars</div>
+          <?php
+            for ($_SESSION['make']=0; $_SESSION['make'] < 30; ++$_SESSION['make']) {
+               if (!(in_array($_SESSION['make'],$_SESSION['ansArr']))) { ?>
+                 <button name="button" value="<?php echo $_SESSION['make'] ?>"><div class="gridBox"><?php echo $_SESSION['qButtons'][$_SESSION['make']] ?></div></button> <?php
+               } else { ?>
+                 <div class="gridBox" Disabled>Disabled</div> <?php
+               }
+            }
+
+            ?>
 
         </div>
         </form>
 
-</div>
-</body>
+
+
+  </body>
 </html>
